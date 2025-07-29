c4a4bf37e6fa23a71e846c770efb5dc7884684c5
Khwaaish <khwaaishdiwan@gmail.com>
2025-07-29 23:41:57 +0530

    Add back to top button #7

--- js/custom.js
+++ js/custom.js
@@ -49,3 +49,15 @@ toggleCheckbox.addEventListener('change', () => {
     applyTheme(theme);
 });
 
+window.onscroll = function () {
+  const btn = document.getElementById("backToTop");
+  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+    btn.style.display = "block";
+  } else {
+    btn.style.display = "none";
+  }
+};
+
+document.getElementById("backToTop").addEventListener("click", function () {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+});
